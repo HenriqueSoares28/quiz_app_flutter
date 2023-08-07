af5ebb848fa2cb363480475808bb7ea1a058359c
Henrique França Carvalho Soares <henrique.soares.2811@gmail.com>
2023-08-07 18:54:04 -0300

    Add functions

--- lib/front_page.dart
+++ lib/front_page.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/styled_text.dart';
 
-import 'package:quiz_app/gradient_container.dart';
 class FrontPage extends StatelessWidget {
-  const FrontPage({super.key});
+  const FrontPage(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
-    return const GradientContainer.purple();
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 40),
+          const StyledText('Learn Flutter the fun way!'),
+          const SizedBox(height: 40),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_forward),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'package:quiz_app/front_page.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: FrontPage(),
-      ),
-    )
-  );
+  runApp(const Quiz());
 }
